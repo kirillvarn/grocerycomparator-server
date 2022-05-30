748bc2f3747b9b3cc6bcb88d7f9d15760ce55759
Kirill Varnatsov <kirill.varnatsov@gmail.com>
2022-05-30 20:39:39 +0000

    server

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import main
+from credentials import api_key
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -8,11 +9,39 @@ cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
 
+@app.route("/")
+@cross_origin()
+def main_page():
+    return "<h1>Hello!</h1>"
+
 @app.route("/dates")
 @cross_origin()
 def index():
     return jsonify(main.get_tables("products"))
 
+
+@app.route("/parse", methods=['POST'])
+@cross_origin()
+def start_parsing():
+    auth = request.headers['Authentication']
+    if auth == api_key:
+        response = main.start_parsing()
+    else:
+        response = {"response": {"status": "error", "message": "You have no access to this!"}}
+
+    return jsonify(response)
+
+
+@app.route("/user", methods=['POST'])
+@cross_origin()
+def user():
+    json = request.get_json()
+    can_login = main.login(json)
+    if can_login == True:
+        response = {"response": {"status": "ok"}}
+    else:
+        response = {"response": {"status": "error", "message": can_login}}
+    return jsonify(response)
 
 @app.route("/products")
 @cross_origin()
@@ -36,6 +65,6 @@ def product_data(id):
 #    return jsonify(main.get_prices(main.conn_naive))
 
 if __name__ == "__main__":
-    from waitress import serve
-
-    serve(app, host="127.0.0.1", port="8080", threads=16)
+    #from waitress import serve
+    app.run()
+    #serve(app, host="0.0.0.0", port="8080", threads=32, url_scheme='https')
