e2ba25f5119494757d491ed286ecee7c585949f8
Kirill Varnatsov <kirill.varnatsov@gmail.com>
2023-01-28 00:46:32 +0200

    fix

--- server.py
+++ server.py
@@ -6,11 +6,15 @@ import parser.naive as naive_parser
 import main
 
 import logging
+import os
 
 FORMAT = "%(asctime)s %(message)s"
 logging.basicConfig(
     filename="log/events.log", encoding="utf-8", format=FORMAT, level=logging.DEBUG
 )
+
+if not os.path.exists("log"):
+    os.mkdir("log/code")
 
 app = Flask(__name__)
 cors = CORS(app)
